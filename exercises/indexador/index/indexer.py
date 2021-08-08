8c40e6d79e6f2435adebae8365aee714c92a35b2
jonathan.candido <jonathan.candido@toroinvestimentos.com.br>
2021-08-08 17:31:30 -0300

    feat: exercises 11 - 14

--- exercises/indexador/index/indexer.py
+++ exercises/indexador/index/indexer.py
@@ -2,20 +2,25 @@ from nltk.stem.snowball import SnowballStemmer
 from bs4 import BeautifulSoup
 import string
 from nltk.tokenize import word_tokenize
+import nltk
 import os
 
+nltk.download('punkt')
 
 class Cleaner:
     def __init__(self,stop_words_file:str,language:str,
                         perform_stop_words_removal:bool,perform_accents_removal:bool,
                         perform_stemming:bool):
+
+        data_path = os.path.dirname(os.path.abspath(__file__))
+        stop_words_file = os.sep.join([data_path] + [stop_words_file])
         self.set_stop_words = self.read_stop_words(stop_words_file)
 
         self.stemmer = SnowballStemmer(language)
-        in_table =  "áéíóúâêôçãẽõü"
-        out_table = "aeiouaeocaeou"
+        in_table =  "áéíóúâêôçãẽõü!?.:;,"
+        out_table = "aeiouaeocaeou      "
         #altere a linha abaixo para remoção de acentos (Atividade 11)
-        self.accents_translation_table = None
+        self.accents_translation_table = in_table.maketrans(in_table,out_table)
         self.set_punctuation = set(string.punctuation)
 
         #flags
@@ -74,11 +79,29 @@ class HTMLIndexer:
     def text_word_count(self,plain_text:str):
         dic_word_count = {}
 
+        plain_text_tokenized = word_tokenize(plain_text)
+
+        for word in plain_text_tokenized:
+            word = self.cleaner.preprocess_word(word)
+            if word not in ["", " "]:
+                if word in dic_word_count:
+                    dic_word_count[word] += 1
+                else:
+                    dic_word_count[word] = 1
         return dic_word_count
+
     def index_text(self,doc_id:int, text_html:str):
-        pass
+        text = HTMLIndexer.cleaner.html_to_plain_text(text_html)
+        dic_words = self.text_word_count(text)
+        for word in dic_words:
+            self.index.index(word, doc_id, dic_words[word])
 
     def index_text_dir(self,path:str):
-
         for str_sub_dir in os.listdir(path):
             path_sub_dir = f"{path}/{str_sub_dir}"
+            ## Condicional escrita para rodar o programa em pastas do MAC OS
+            if str_sub_dir not in '.DS_Store':
+                for file_name in os.listdir(path_sub_dir):
+                    file_path = f"{path_sub_dir}/{file_name}"
+                    with open(file_path, "rb") as file:
+                        self.index_text(int((file_name.split("."))[0]), file)
